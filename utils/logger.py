ab0e17370e860ebecf5c4f419f02da70762af2e1
AdityaSinghDevs <adityasingh.devs@gmail.com>
2025-10-02 11:08:12 +0530

    Loggers setup

---
+++ utils/logger.py
@@ -0,0 +1,39 @@
+import logging 
+import os 
+from typing import Optional
+
+def get_logger(name: str,
+               log_level: str = 'INFO',
+               console : bool = True,
+               log_file : Optional[str] = None)->logging.logger:
+    
+    logger = logging.getLogger(name)
+
+    logger.setLevel(getattr(logging, log_level.upper(), logging.INFO))
+
+    if not logger.hasHandlers():
+
+        if console:
+            console_handler = logging.StreamHandler()
+
+            console_format = logging.Formatter("{name}-{levelname} - {message}" , style='{')
+
+            console_handler.setFormatter(console_format)
+            logger.addHandler(console_handler)
+
+    base_log_dir = os.path.join(os.path.dirname(__file__), "..", "logs")
+    base_log_dir = os.path.abspath(base_log_dir)
+
+    if log_file:
+
+        log_file = os.path.join(base_log_dir, log_file)
+        os.makedirs(os.path.dirname(log_file), exist_ok=True)
+
+        file_handler = logging.FileHandler(log_file , mode='a', encoding='utf-8')
+
+        log_format = logging.Formatter("{asctime} - {levelname} - {name}:{funcname}:L{lineno}:{message}", style = '{')
+
+        file_handler.setFormatter(log_format)
+        logger.addHandler(file_handler)
+
+    return logger
